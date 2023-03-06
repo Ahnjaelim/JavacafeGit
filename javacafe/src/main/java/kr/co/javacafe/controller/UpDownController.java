2fb8fae403b59cac87dba34777bb131dfc4dbfd1
letsgomove <kycoco018@naver.com>
2023-03-06 10:38:32 +0900

    updowncontroller upload삭제 / sidebar 텍스트충돌난거 수정

--- javacafe/src/main/java/kr/co/javacafe/controller/UpDownController.java
+++ javacafe/src/main/java/kr/co/javacafe/controller/UpDownController.java
@@ -42,59 +42,6 @@ public class UpDownController {
 
 
 	@Value("${upload.path}")
-	private String iuploadPath;
-	
-	@ApiOperation(value = "Upload POST", notes = "POST방식으로 파일등록")
-												//springframwork/http/ import
-	@PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public List<UploadResultDTO> upload(UploadFileDTO UploadFileDTO) {
-		
-		log.info(UploadFileDTO);
-		
-		if(UploadFileDTO.getFiles() != null) {
-			
-			final List<UploadResultDTO> list = new ArrayList<>();
-			
-			UploadFileDTO.getFiles().forEach(multipartFile -> {
-				
-				String originalName = multipartFile.getOriginalFilename();
-				log.info(originalName);
-				
-				String uuid = UUID.randomUUID().toString();
-				
-				//nio.Path import
-				Path savePath = Paths.get(iuploadPath, uuid+"_"+originalName);
-				
-				boolean image = false;
-				
-				try {
-					multipartFile.transferTo(savePath); // 실제 파일저장
-					
-					//이미지 파일의 종류라면 (썸네일만들기) tumbnailator라이브러리 사용
-					if(Files.probeContentType(savePath).startsWith("image")) {
-						File thumbFile = new File(iuploadPath, "s_" + uuid+"_"+originalName);
-						Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200,200);
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}//tryend
-				
-				list.add(UploadResultDTO.builder()
-						.uuid(uuid)
-						.fileName(originalName)
-						.img(image).build()
-						);
-				
-			}); //foreach end
-			
-			return list;
-		} // if end
-			
-		
-		return null;
-	}
-
-	@Value("${upload.path}")
 	private String uploadPath;
 	
 	@GetMapping("/view/{fileName}")
