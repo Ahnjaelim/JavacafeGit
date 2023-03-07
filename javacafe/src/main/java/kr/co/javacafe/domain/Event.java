e1085a3d4333f0c7a0c1106edf1ebececbf9c6e7
jominhyeok94 <alsgur140825@gmail.com>
2023-03-07 12:41:40 +0900

    이벤트 게시판 기본 CRUD & 게시글 작성시 00번 게시물 등록되었습니다 라는 모달창 뜨게 적용

--- javacafe/src/main/java/kr/co/javacafe/domain/Event.java
+++ javacafe/src/main/java/kr/co/javacafe/domain/Event.java
@@ -3,7 +3,7 @@ package kr.co.javacafe.domain;
 import lombok.*;
 import javax.persistence.*;
 
-// 엔티티 객체를 위한 엔티티 클래스는 반드시 @entity를 적용해야하고 @id가 필요하다
+
 @Entity
 @Getter
 @Builder
@@ -12,8 +12,7 @@ import javax.persistence.*;
 @ToString
 public class Event extends BaseEntity {
 
-	// 게시물은 데이터베이스에 추가될 때 생성되는 번호(auto increment)를 이용할 것이므로
-	// 키 생성전략(key generate strategy)중에 GenerationType.IDENTITY로 데이터베이스에서 알아서 결정하는 방식을 이용한다.
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long eno;
--- javacafe/src/main/java/kr/co/javacafe/repository/search/BoardSearch.java
+++ javacafe/src/main/java/kr/co/javacafe/repository/search/BoardSearch.java
@@ -2,12 +2,14 @@ package kr.co.javacafe.repository.search;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
 import kr.co.javacafe.domain.Board;
+
 
 public interface BoardSearch {
 
-    Page<Board> search1(Pageable pageable);
-
-    Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
+	Page<Board> search1(Pageable pageable);
+	
+	
+	Page<Board> searchAll(String [] types, String keyword, Pageable pageable);
+	
 }
