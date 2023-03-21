adf3ac2371e940ab5bfa352d446b9b893c1ae294
letsgomove <kycoco018@naver.com>
2023-03-21 17:25:44 +0900

    공백 및 쓸데없는 import삭제

--- javacafe/src/main/java/kr/co/javacafe/repository/search/InvenSearch.java
+++ javacafe/src/main/java/kr/co/javacafe/repository/search/InvenSearch.java
@@ -3,9 +3,9 @@ package kr.co.javacafe.repository.search;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import kr.co.javacafe.domain.Admin;
+ 
 import kr.co.javacafe.domain.Inventory;
-import kr.co.javacafe.domain.Sales;
+ 
 
 public interface InvenSearch {
 
